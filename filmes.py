b924cf9e4ba86be69f598e8812be5e8f3316d762
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-05-26 12:01:52 -0300

    Add bar chart for director gender representation

--- filmes.py
+++ filmes.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Gráfico no Streamlit")
 
@@ -7,14 +8,22 @@ st.write("Este site analisa uma lista de filmes gerada por uma IA (ChatGPT) e an
 
 df = pd.read_csv("filmes.csv")
 
+st.subheader("Tabela de Dados")
+st.dataframe(df)
+
 contagem = df["Gênero Diretor"].value_counts()
 
 st.subheader("Diretores Femininos x Masculinos")
 
+
+fig, ax = plt.subplots()
+
 ax.bar(contagem.index, contagem.values)
+
 
 ax.set_xlabel("Gênero")
 ax.set_ylabel("Quantidade")
 ax.set_title("Quantidade de Diretores por Gênero")
+
 
 st.pyplot(fig)
