22977abbc9e5f8b27a44b7762c55991787c5dfd5
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-05-26 11:51:50 -0300

    Create filmes.py

---
+++ filmes.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("Gráfico no Streamlit")
+
+st.write("Este site analisa uma lista de filmes gerada por uma IA (ChatGPT) e analisa a representatividade feminina nas IAs"
+
+df = pd.read_csv("filmes.csv")
+
+contagem = df["Gênero Diretor"].value_counts()
+
+st.subheader("Diretores Femininos x Masculinos")
+
+fig, ax = plt.subplots()
+
+ax.bar(contagem.index, contagem.values)
+
+ax.set_xlabel("Gênero")
+ax.set_ylabel("Quantidade")
+ax.set_title("Quantidade de Diretores por Gênero")
+
+st.pyplot(fig)
